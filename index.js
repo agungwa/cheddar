3f2c618c5ea77004586026737d0ff04a819b0ede
agungw <agung.widhiatmojo@gmail.com>
2021-05-06 23:02:45 +0700

    handle binary code json

--- index.js
+++ index.js
@@ -21,42 +21,54 @@ const {
 //       });
 //     });
 // }
-async function getData( url ) {
-    const endpoint = url;
-    const config = {
-        headers: {
-        "Content-Type": "application/json",
-        },
-    };
-    // Add Your Key Here!!!
-    // axios.defaults.headers.common = {
-    //     "x-api-key": "dPnKIbgQLJaezOPWvOTQkaBPBjMiCyRy7Dnw3zDL",
-    // };
-    const data = await axios({
-        method: "get",
-        url: endpoint,
-        config,
-    })
-    console.log(data)
-};
 
-// let job = new CronJob('5/5 9-23 * * *', async function() {
+// async function getData( url ) {
+//     const endpoint = url;
+//     const config = {
+//         headers: {
+//         "Content-Type": "application/json",
+//         },
+//     };
+//     // Add Your Key Here!!!
+//     // axios.defaults.headers.common = {
+//     //     "x-api-key": "dPnKIbgQLJaezOPWvOTQkaBPBjMiCyRy7Dnw3zDL",
+//     // };
+//     const data = await axios({
+//         method: "get",
+//         url: endpoint,
+//         config,
+//     })
+//     console.log(data)
+// };
+
+async function getData( endpoint ) {
+    let url = endpoint;
+    const response = await axios.get(url)
+    return response
+}
+
+// let job = new CronJob('10/10 9-23 * * 1-5', async function() {
 let lte_ny = ny.subtract(10,'minutes');
 let lte = lte_ny.format('HH:mm');
 let gte_ny = ny.subtract(10,'minutes');
 let gte = gte_ny.format('HH:mm');
-let runJob = async () => { new CronJob('10/10 9-23 * * 1-5', async function() {
+let runJob = async () => { new CronJob('02 12 * * 1-5', async function() {
         if (momentZone().tz("America/New_York").format('HH:mm') > '09:49'){
             let endpoint = `http://13.250.47.31:3000/fetch?cmd=TOP&symbol=@top100&limit=500&time_gte=${gte}&time_lte=${lte}`
-            let json = getData( endpoint);
-            let jsonData = json.data;
-            // console.log( json.data )
+            let dateNow = momentZone().tz("America/New_York").format("YYYY-MM-DD HH:mm ZZ");
+            console.log( endpoint );
+            let json = await getData( endpoint);
+            // console.log( json )
+            let jsonParse = json.data;
+            let jsonData = jsonParse.data;
+            // console.log( jsonData )
+            let count = 0;
             for ( let dataIngest of jsonData ){
-                console.log( dataIngest )
+                count ++;
                 await handleUpsertData( elasticClient, dataIngest );
             }
-            let dateNow = momentZone().tz("America/New_York").format("YYYY-MM-DD HH:mm ZZ");
-            console.log( gte,' - ',lte, ' now: ',dateNow);
+            
+            console.log( gte,` - `,lte, `hit data total ${count} now: `,dateNow);
             console.log( endpoint );
         } else {
             console.log(`time < 09.50`, ' now: ',momentZone().tz("America/New_York").format('HH:mm'))
